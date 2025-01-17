59e358b3a8e6f32558af600f2f10efbd39c95398
hasankarim18 <hallysust@gmail.com>
2025-01-17 23:52:01 +0600

    model viewr aspect ratio

--- assets/product-modal.js
+++ assets/product-modal.js
@@ -8,13 +8,36 @@ class ProductModelModal extends HTMLElement {
     this.openModelModal();
   }
 
-  getMediaId() {}
+  getMediaId() {
+    let mediaId = this.dataset.mediaId;
+    return mediaId;
+  }
 
-  getModel() {}
+  getModel() {
+    return document.getElementById("productModelModal");
+  }
 
   openModelModal() {
     const mediaId = this.getMediaId();
+    //   console.log(mediaId);
     const modal = this.getModel();
+
+    if (!mediaId) return;
+
+    const openModelButton = this.querySelector(
+      `button[id="productMoelModalOpenButton_${mediaId}"]`
+    );
+    openModelButton.addEventListener("click", (e) => {
+      const modalBody = modal.querySelector("#body");
+
+      modalBody.innerHTML = "";
+      const template = this.querySelector("template");
+      const templateClone = template.content.cloneNode(true);
+      modalBody.appendChild(templateClone);
+      const modelViewer = modal.querySelector("model-viewer");
+      modelViewer.setAttribute("reveal", "auto");
+      // modelViewer.className = "w-[70vw] h-[70vw] md:w-[50vw] md:h-[50vw]";
+    });
   }
 }
 
